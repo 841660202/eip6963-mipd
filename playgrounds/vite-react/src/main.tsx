f275c93b2a3e19b75a148b944a92712784af7c8b
haotian.chen <haotian.chen@cmexpro.com>
2024-06-03 15:56:59 +0800

    fix: json

--- playgrounds/vite-react/src/main.tsx
+++ playgrounds/vite-react/src/main.tsx
@@ -1,14 +1,14 @@
-import { createStore } from 'mipd'
-import { useSyncExternalStore } from 'react'
-import ReactDOM from 'react-dom/client'
+import { createStore } from 'mipd';
+import { useSyncExternalStore } from 'react';
+import ReactDOM from 'react-dom/client';
 
-const store = createStore()
+const store = createStore();
 
 export default function App() {
-  const providers = useSyncExternalStore(store.subscribe, store.getProviders)
-  return <pre>{JSON.stringify(providers, null, 2)}</pre>
+  const providers = useSyncExternalStore(store.subscribe, store.getProviders);
+  console.log('providers', providers);
+  const _providers = providers.map((item) => item.info);
+  return <pre>{JSON.stringify(_providers || {}, null, 2)}</pre>;
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
